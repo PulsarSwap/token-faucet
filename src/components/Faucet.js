5bafd8d6a3a0b1fdc42981db35c8659dcaa5610c
skyve2012 <hongyushen21@hotmail.com>
2022-12-23 23:21:39 -0500

    fix button issue

--- src/components/Faucet.js
+++ src/components/Faucet.js
@@ -5,11 +5,10 @@ import Button from "react-bootstrap/Button";
 import { TSPUL_ADDRESS, TSUSD_ADDRESS } from "../constants";
 import Message from "./Message";
 
-const Faucet = ({ props, tokenAddress }) => {
+const Faucet = (props) => {
   const [balance, setBalance] = useState();
   const [showBalance, setShowBalance] = useState(false);
-
-  async function getBalance() {
+  async function getBalance(tokenAddress) {
     if (typeof window.ethereum !== "undefined") {
       const [account] = await window.ethereum.request({
         method: "eth_requestAccounts",
@@ -17,17 +16,17 @@ const Faucet = ({ props, tokenAddress }) => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(
         tokenAddress,
-        props.tokenContract.abi,
+        props.tokenContract,
         provider
       );
-      const balance = await contract.balanceOf(account[0]);
-      console.log("Balance: ", balance.toString());
+      const balance = await contract.balanceOf(account);
+      // console.log("Balance: ", balance.toString());
       setBalance(balance.toString());
       setShowBalance(true);
     }
   }
 
-  async function faucet({ props, tokenAddress }) {
+  async function faucet(tokenAddress) {
     if (typeof window.ethereum !== "undefined") {
       // const account = await window.ethereum.request({
       //   method: "eth_requestAccounts",
@@ -36,7 +35,7 @@ const Faucet = ({ props, tokenAddress }) => {
       const signer = provider.getSigner();
       const contract = new ethers.Contract(
         tokenAddress,
-        props.tokenContract.abi,
+        props.tokenContract,
         signer
       );
       contract.mintToken();
@@ -56,16 +55,16 @@ const Faucet = ({ props, tokenAddress }) => {
         <Card.Title>Goerli ERC20-Token Faucet</Card.Title>
         <br></br>
         <div className="d-grid gap-2">
-          <Button onClick={faucet(TSUSD_ADDRESS)}>Mint 10,000 TSUSD</Button>{" "}
-          <Button onClick={getBalance(TSUSD_ADDRESS)} variant="warning">
+          <Button onClick={() => faucet(TSUSD_ADDRESS)}>Mint 10,000 TSUSD</Button>{" "}
+          <Button onClick={() => getBalance(TSUSD_ADDRESS)} variant="warning">
             Check TSUSD Balance
           </Button>
           {showBalance ? <Message balance={balance} /> : null}
         </div>
         <br></br>
         <div className="d-grid gap-2">
-          <Button onClick={faucet(TSPUL_ADDRESS)}>Mint 10,000 TSPUL</Button>{" "}
-          <Button onClick={getBalance(TSPUL_ADDRESS)} variant="warning">
+          <Button onClick={() => faucet(TSPUL_ADDRESS)}>Mint 10,000 TSPUL</Button>{" "}
+          <Button onClick={() =>getBalance(TSPUL_ADDRESS)} variant="warning">
             Check TSPUL Balance
           </Button>
           {showBalance ? <Message balance={balance} /> : null}
